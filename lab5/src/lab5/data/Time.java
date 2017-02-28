a3a76889483f7eeff396ef6a7b360d621b9ab1fb
inaule-6 <inaule-6@rattata.sts.ltu.se>
2017-02-28 14:55:51 +0100

    time

--- lab5/src/lab5/data/Time.java
+++ lab5/src/lab5/data/Time.java
@@ -1,5 +1,32 @@
 package lab5.data;
 
+/**
+ * Inte färdig
+ * @author inaule-6
+ *
+ */
 public class Time {
-
+	private int elapsedTime;
+	private int lastEventTime;
+	
+	public Time() {
+	elapsedTime = 0;
+	lastEventTime = elapsedTime;
+	}
+	
+	public void addTime(int t) {
+		elapsedTime += t;
+	}
+	
+	public int getElapsedTime() {
+		return elapsedTime;
+	}
+	
+	public void resetLastEventTime() {
+		lastEventTime = elapsedTime;
+	}
+	
+	public int getTempTime() {
+		return lastEventTime;
+	}
 }
