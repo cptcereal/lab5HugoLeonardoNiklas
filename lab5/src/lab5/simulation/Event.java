3577bdeccaebbdfd595645f47c1383df973b57be
Leo <Leo@130.240.158.237>
2017-02-28 14:52:53 +0100

    Event

--- lab5/src/lab5/simulation/Event.java
+++ lab5/src/lab5/simulation/Event.java
@@ -4,8 +4,9 @@ public abstract class Event {
 	private final int TIME;
 	private Simulation sim;
 	
-	public Event(int time, Simulation sim){
-		
+	public Event(Simulation sim, int time){
+		sim = this.sim;
+		TIME = this.time;
 	}
 	
 	public void effect(State state){
