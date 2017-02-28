972c09fe31e41538bf68037b33d08e4eb585ed71
inaule-6 <inaule-6@rattata.sts.ltu.se>
2017-02-28 15:19:11 +0100

    time , state

--- lab5/src/lab5/data/State.java
+++ lab5/src/lab5/data/State.java
@@ -1,5 +1,38 @@
 package lab5.data;
 
-public class State {
+import java.util.Observable;
 
+/**
+ * Has the state of the simulator, info about time and running.
+ * @author inaule-6
+ *
+ */
+public class State extends Observable {
+	
+	private Time time;
+	private boolean stop;
+	
+	/**
+	 * Makes state object.
+	 */
+	public State(){
+		time = new Time();
+		stop = false;
+	}
+	
+	/**
+	 * Sets stop to true. 
+	 * Stop makes it impossible to make new events.
+	 */
+	public void stop() {
+		stop = true;
+	}
+	
+	/**
+	 * Returns the elapsedTime of simulator
+	 * @return
+	 */
+	public int getElapsedTime(){
+		return time.getElapsedTime();
+	}
 }
--- lab5/src/lab5/data/Time.java
+++ lab5/src/lab5/data/Time.java
@@ -1,7 +1,7 @@
 package lab5.data;
 
 /**
- * Inte färdig
+ * Saves elapsed time and the time of last event.
  * @author inaule-6
  *
  */
@@ -9,24 +9,42 @@ public class Time {
 	private int elapsedTime;
 	private int lastEventTime;
 	
+	/**
+	 * Makes time object
+	 */
 	public Time() {
 	elapsedTime = 0;
 	lastEventTime = elapsedTime;
 	}
 	
+	/**
+	 * Adds time to elapsed time.
+	 * @param t
+	 */
 	public void addTime(int t) {
 		elapsedTime += t;
 	}
 	
+	/**
+	 * Returns the elapsed time.
+	 * @return
+	 */
 	public int getElapsedTime() {
 		return elapsedTime;
 	}
 	
+	/**
+	 * Sets lastEventTime to elapsedTime.
+	 */
 	public void resetLastEventTime() {
 		lastEventTime = elapsedTime;
 	}
 	
-	public int getTempTime() {
+	/**
+	 * Returns lastEventTime.
+	 * @return
+	 */
+	public int getLastEventTime() {
 		return lastEventTime;
 	}
 }
