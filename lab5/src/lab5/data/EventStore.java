d0159dde9b45abd45ec85ca17537380eee590cc9
inaule-6 <inaule-6@rattata.sts.ltu.se>
2017-02-28 11:35:58 +0100

    dsasdass

--- lab5/src/lab5/data/EventStore.java
+++ lab5/src/lab5/data/EventStore.java
@@ -1,6 +1,34 @@
 package lab5.data;
 
+import java.util.ArrayList;
+import lab5.simulation.Event;
+
+/**
+ * This class Stores events and
+ * 
+ * @author inaule-6
+ * 
+ */
+
 public class EventStore {
-//sadsasdads
+	private ArrayList<Event> eventStore = new ArrayList<Event>();
 	
+	public EventStore(Event[] startEvents) {
+		for (int i = 0; i < startEvents.length; i++) {
+			eventStore.add(startEvents[i]);
+		}
+	}
+
+	public void add(Event e){
+		eventStore.add(e);
+	}
+	
+	public Event nextEvent(){
+		if (eventStore.isEmpty()){
+			throw new IndexOutOfBoundsException("EventStore is empty");
+		}
+		Event temp = eventStore.get(0);
+		eventStore.remove(0);
+		return temp;
+	}
 }
