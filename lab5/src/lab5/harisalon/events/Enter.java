d90759bf3e912dba65b7bc0211a93d5b28e18235
hugow <hugow@DESKTOP-J9R1RO0>
2017-02-28 16:03:01 +0100

    kkl

--- lab5/src/lab5/harisalon/events/Enter.java
+++ lab5/src/lab5/harisalon/events/Enter.java
@@ -1,5 +1,19 @@
 package lab5.harisalon.events;
 
-public class Enter {
+import lab5.simulation.Event;
+import lab5.simulation.Simulation;
+
+/**
+ * The event representing that a customer enters the hair salon
+ * 
+ * @author hugwan-6, leopel-6, inaule-6
+ *
+ */
+public class Enter extends Event{
+
+	public Enter(Simulation sim, int time) {
+		super(sim, time);
+		
+	}
 
 }
