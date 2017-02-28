d5dcd2ef2b1d84eb8e13bc5cdc9e46535327e7ad
hugow <hugow@DESKTOP-J9R1RO0>
2017-02-28 14:57:44 +0100

    Simulation update

--- lab5/src/lab5/simulation/Simulation.java
+++ lab5/src/lab5/simulation/Simulation.java
@@ -1,18 +1,37 @@
 package lab5.simulation;
 
 import lab5.data.EventStore;
+import lab5.data.State;
+import lab5.gui.View;
 
+/**
+ * Runs the simulation
+ * 
+ * @author hugwan-6, leopel-6, inaule-6
+ *
+ */
 public class Simulation {
 	
 	private View view;
 	private State state;
 	private EventStore eventStore;
 	
+	/**
+	 * Starts up the simulation
+	 * 
+	 * @param events the event array that contains at least one start event for the simulation.
+	 */
 	public Simulation(Event[] events) {
+		state = new State();
+		view = new View();	// WIP
 		eventStore = new EventStore(events);
 		run();
 	}
 	
+	/**
+	 * Keeps the simulation running until it's done
+	 * 
+	 */
 	public void run() {
 		while(!eventStore.isEmpty) {
 			eventStore.nextEvent();
