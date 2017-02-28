87b532772e4c09e1fe25018dcba744819d0c0d30
Leo <Leo@130.240.158.237>
2017-02-28 15:44:25 +0100

    cms

--- lab5/src/lab5/simulation/Event.java
+++ lab5/src/lab5/simulation/Event.java
@@ -2,24 +2,45 @@ package lab5.simulation;
 import lab5.data.State;
 import lab5.data.Time;
 
-
+/**
+ * Creates events based with a specified time and simulator
+ * @author Leo
+ * 
+ */
 public abstract class Event {
 	private final int TIME;
 	private Simulation sim;
 	
+	/**
+	 * Creates an Event object with specified simulation and time values
+	 * @param sim
+	 * @param time
+	 */
 	public Event(Simulation sim, int time){
 		sim = this.sim;
-		TIME = this.time;
+		this.TIME = time;
 	} 
 	
+	/**
+	 * Makes a call to a specific Event depending on the specified state
+	 * @param state
+	 */
 	public void effect(State state){
 		
 	}
 	
+	/**
+	 * Returns the value of a time for an object
+	 * @return
+	 */
 	public int getTime(){
 		return TIME;
 	}
 	
+	/**
+	 * Returns the value the simulation value for an object
+	 * @return
+	 */
 	public Simulation getSim(){
 		return sim;
 	}
