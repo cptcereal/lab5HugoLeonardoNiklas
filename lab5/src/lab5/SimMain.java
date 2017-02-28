01c2524634124755295a6ad6f8332e4e15bf0c5c
hugow <hugow@DESKTOP-J9R1RO0>
2017-02-28 11:01:39 +0100

    SimMain

--- lab5/src/lab5/SimMain.java
+++ lab5/src/lab5/SimMain.java
@@ -1,12 +1,25 @@
 package lab5;
 
 import lab5.simulation.Event;
+import lab5.simulation.Simulation;
 
+/**
+ * Creates a new simulation
+ * 
+ * @author hugwan-6, leopel-6, inaule-6
+ */
 public class SimMain {
 
 	private Simulation sim;
 	
-	public static void main(Event args) {
+	/**
+	 * Starts the simulation with the start events
+	 * 
+	 * @param arg the start argument that contains the start events
+	 */
+	public static void main(Event[] arg) {
+		
+		sim = new Simulation(arg);
 		
 	}
 }
