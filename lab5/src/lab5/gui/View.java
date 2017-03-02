b3ccb0009922bf0501b0513dd8328028c344b7d7
leo <leo@leo-Aspire-E5-522>
2017-03-02 14:45:01 +0100

    view

--- lab5/src/lab5/gui/View.java
+++ lab5/src/lab5/gui/View.java
@@ -1,7 +1,11 @@
 package lab5.gui;
 
 
-
+/**
+ * Displays the corresponding data to a specific simulation,i.e. important information crucial to the simulation
+ * @author hugwan-6, leopel-6, inaule-6 
+ *
+ */
 public class View {
 	
 	public View() {
