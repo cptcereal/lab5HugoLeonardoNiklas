1a5d919f34f736b0fa24f624506b69a9895b9e2d
inaule-6 <inaule-6@pidgey.sts.ltu.se>
2017-03-02 13:12:01 +0100

    gfd

--- lab5/src/lab5/hairsalon/data/QueueList.java
+++ lab5/src/lab5/hairsalon/data/QueueList.java
@@ -19,7 +19,7 @@ public class QueueList {
 	}
 	
 	public boolean addToQueue(Enter e) {
-		if (lastInQueue != queue.length) {
+		if (lastInQueue + lastInVIPQueue < queue.length) {
 			queue[lastInQueue] = e;
 			lastInQueue += 1;
 			return true;
@@ -28,24 +28,35 @@ public class QueueList {
 	}
 	
 	public boolean addToVIPQueue(Dissatisfied e) {
-		if (lastInVIPQueue != vipQueue.length) {
+		if (lastInVIPQueue < vipQueue.length) {
 			vipQueue[lastInQueue] = e;
 			lastInVIPQueue += 1;
-			
+			if (lastInQueue + lastInVIPQueue >= queue.length) {
+				lastInQueue -= 1;
+			}
 			return true;
 		}
 		return false;
 	}
 	
-	private void addVIPToQueue() {
-		for (int i = 0; i < lastInQueue; i++) {
-			queue[0:-1] = queue
-			queue[i] = vipQueue[i];
+	public Event next() {
+		if (lastInVIPQueue != 0) {
+			lastInVIPQueue -= 1;
+			Event temp =vipQueue[0];
+			for (int i = 0; i < lastInVIPQueue; i++) {
+				vipQueue[i] = vipQueue[i + 1];
+			}
+			return temp;
 		}
+		Event temp = queue[0];
+		lastInQueue -= 1;
+		for (int i = 0; i < lastInQueue; i++) {
+			queue[i] = queue[i + 1];
+		}
+		return temp;
 	}
 	
-	public Event next() {
-		return queue[0];
+	public boolean isEmpty() {
+		return (lastInQueue == 0 && lastInVIPQueue == 0);
 	}
-
 }
