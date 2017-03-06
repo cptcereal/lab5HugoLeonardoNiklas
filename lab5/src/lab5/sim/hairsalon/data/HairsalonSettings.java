5c7cbb6b3613134742ebe1efe5856babb4017e9f
hugow <hugow@DESKTOP-J9R1RO0>
2017-03-06 10:48:29 +0100

    123

--- lab5/src/lab5/sim/hairsalon/data/HairsalonSettings.java
+++ lab5/src/lab5/sim/hairsalon/data/HairsalonSettings.java
@@ -15,22 +15,22 @@ public class HairsalonSettings implements PrintAble{
 	private final double D_MAX = 200;
 	private final double P = 0.2;
 	private final double CLOSING_TIME = 3000;
-	private final double CUSTOMERS_PER_HOUR = 5;
+	private final double CUSTOMERS_PER_TIMEUNIT = 5;
 	private final int MAX_QUEUE = 5;
 	private final int MAX_CHAIRS = 3;
 	private final long SEED = System.currentTimeMillis();
 	
 	public void printAll() {
-		System.out.println("H min = " + H_MIN);
-		System.out.println("H max = " + H_MAX);
-		System.out.println("D min = " + D_MIN );
-		System.out.println("D max = " + D_MAX);
-		System.out.println("P = " + P);
-		System.out.println("Seed =" + SEED);
-		System.out.println("Closing time = " + CLOSING_TIME);
-		System.out.println("Customers per hour = " + CUSTOMERS_PER_HOUR);
-		System.out.println("Max queue = " + MAX_QUEUE);
-		System.out.println("Max chairs = " + MAX_CHAIRS);
+		System.out.println("Closing time of the day ..............: " + CLOSING_TIME);
+		System.out.println("Total number of chairs ...............: " + MAX_CHAIRS);
+		System.out.println("Maximum queue size ...................: " + MAX_QUEUE);
+		System.out.println("Lambda (customers/timeunit entering)..: " + CUSTOMERS_PER_TIMEUNIT);
+		System.out.println("hmin and hmax (cutting time interval) : [" + H_MIN + ", " + H_MAX + "]");
+		System.out.println("dmin and dmax (return time interval) .: [" + D_MIN + ", "+ D_MAX + "]");
+		System.out.println("Risk dissatisfied returns: ...........: " + P);
+		System.out.println("Seed used in pseudo random generator .: " + SEED);
+		System.out.println("----------------------------------------------------------------");
+		System.out.println("- Time  Event      Id  Idle  TIdle  TWait  InQ  Cut  Lost  Ret -");
 	}
 	
 	/**
@@ -92,8 +92,8 @@ public class HairsalonSettings implements PrintAble{
 	 * 
 	 * @return customers per hour
 	 */
-	public double getCustomersPerHour() {
-		return CUSTOMERS_PER_HOUR;
+	public double getCustomersPerTimeUnit() {
+		return CUSTOMERS_PER_TIMEUNIT;
 	}
 	
 	/**
