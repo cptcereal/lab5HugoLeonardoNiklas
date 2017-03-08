3c5e7e15df95fb262b58c760e3e907bd92138bb0
hugow <hugow@DESKTOP-J9R1RO0>
2017-03-08 10:33:44 +0100

    123

--- lab5/src/lab5/sim/hairsalon/data/Stop.java
+++ lab5/src/lab5/sim/hairsalon/data/Stop.java
@@ -4,12 +4,29 @@ import lab5.sim.general.data.Event;
 import lab5.sim.general.data.Simulation;
 import lab5.sim.general.data.Time;
 
+/**
+ * Represents the event that the hair salon simulation ends
+ * 
+ * @author hugwan-6, leopel-6, inaule-6
+ *
+ */
 public class Stop extends Event {
 
+	/**
+	 * Creates the event and assigns it a simulation, time and customer
+	 * 
+	 * @param sim - the simulation the event effects
+	 * @param time - the time that the event occurs
+	 * @param customer - the customer the event belongs to
+	 */
 	public Stop(Simulation sim, Time time) {
 		super(sim, time);
 	}
 	
+	/**
+	 * The effect the stop event has on the state of the simulation when it occurs
+	 * 
+	 */
 	public void effect() {
 		HairsalonState state = ((HairsalonState)getSim().getState());
 		state.addtimewaiting(this);
