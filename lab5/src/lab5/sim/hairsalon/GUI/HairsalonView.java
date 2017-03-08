fbee1ac056a002a4476e0548fa76ba068da0d149
hugow <hugow@DESKTOP-J9R1RO0>
2017-03-08 13:14:56 +0100

    123

--- lab5/src/lab5/sim/hairsalon/GUI/HairsalonView.java
+++ lab5/src/lab5/sim/hairsalon/GUI/HairsalonView.java
@@ -1,5 +1,14 @@
 package lab5.sim.hairsalon.GUI;
 
-public class HairsalonView {
+import java.util.Observable;
+import java.util.Observer;
+
+public class HairsalonView implements Observer {
+	
+	
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
