fc7fcd91b4ee2c82b2e6b79ab6e7ca103d4056ae
hugow <hugow@DESKTOP-J9R1RO0>
2017-03-08 10:31:37 +0100

    123

--- lab5/src/lab5/sim/hairsalon/data/StateInfo.java
+++ lab5/src/lab5/sim/hairsalon/data/StateInfo.java
@@ -23,6 +23,20 @@ public class StateInfo implements PrintAble {
 	public final Event event;
 	public final int numHaircut;
 	
+	/**
+	 * The constructor takes in all the information about that simulation that we are interested in displaying
+	 * 
+	 * @param settings - the settings of the simulation
+	 * @param customerList 
+	 * @param haircutList
+	 * @param queueList
+	 * @param timeIdle - the time chairs in the hair salon was idle
+	 * @param timeWaiting - the total waiting time of the customers
+	 * @param elapsedTime - the time of the simulation
+	 * @param event
+	 * @param numHaircut
+	 * @param dissatisfiedlist
+	 */
 	public StateInfo(HairsalonSettings settings, CustomerList customerList, CustomerList haircutList, QueueList queueList, 
 			Time timeIdle, Time timeWaiting, Time elapsedTime, Event event, int numHaircut, CustomerList dissatisfiedlist){
 		
@@ -38,38 +52,77 @@ public class StateInfo implements PrintAble {
 		this.timeIdle = timeIdle;
 	}
 	
+	/**
+	 * 
+	 * @return number of customers in the queue
+	 */
 	private int numWaiting() {
 		return queueList.getQueueSize();
 	}
 	
+	/**
+	 * 
+	 * @return number of lost customers
+	 */
 	private int numLost(){
 		return queueList.getLost();
 	}
 	
+	/**
+	 * 
+	 * @return number of idle chairs in the hair salon
+	 */
 	private int idleChairs(){
 		return (settings.getMaxChairs() - numHaircut);
 	}
 	
+	/**
+	 * 
+	 * @return number of customers that have been cut in the hair salon
+	 */
 	private int numCut(){
 		return haircutList.numCustomers();
 	}
 	
+	/**
+	 * 
+	 * @return the name of the event
+	 */
 	private String eventName(){
 		return event.toString();
 	}
 	
+	/**
+	 * 
+	 * @return idle time of the chairs in the hair salon
+	 */
 	private double tIdle(){
 		return timeIdle.getElapsedTime(); 
 	}
 	
+	/**
+	 * Returns the average cutting time of all the customers who got their hair cut during the simulation
+	 * 
+	 * @return average cutting time
+	 */
 	private double averageCuttingTime() {
 		return ((elapsedTime.getElapsedTime() * settings.getMaxChairs()) - tIdle()) / numCut();
 	}
 	
+	/**
+	 * Returns the average queue time of all the customers who got their hair cut during the simulation
+	 * 
+	 * @return average queue time
+	 */
 	private double averageQueueTime() {
 		return (timeWaiting.getElapsedTime() / numCut());
 	}
 	
+	/**
+	 * Returns the ID of the customer the event belongs to
+	 * 
+	 * @return the ID of the customer
+	 */
 	private int customerID(){
 		if(event.getClass().equals(Enter.class)) {
 			return ((Enter) event).getCustomer().getID();
@@ -79,7 +132,11 @@ public class StateInfo implements PrintAble {
 			return ((Return) event).getCustomer().getID();
 		}
 	}
-
+	
+	/**
+	 * Updates the information about the state depending on what event that happens
+	 * 
+	 */
 	public void printAll() {
 		
 		if (event.getClass().equals(Start.class)) {
